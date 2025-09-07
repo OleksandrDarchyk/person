336327cd984501d3baa3eab996e726bdcb7ec1fa
OleksandrDarchyk <oledar01@easv365.dk>
2025-09-07 22:38:03 +0200

    CRUD

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -5,7 +5,27 @@ namespace ph.Controllers;
 public class PersonController: ControllerBase
 
 {
+    
+    [HttpPut]
+    [Route("/person/{id}")]
+    public Person UpdatePerson([FromRoute] int id, [FromBody] Person person)
+    {
+        var existigPerson = FakeDB.db.First(p => p.Id == id);
+        existigPerson.Name = person.Name;
+        return existigPerson;
+    }
+    
+    [HttpGet]
+    [Route ("api/person")]
+    public List<Person> GetPersons()
+    {
+        return FakeDB.db.ToList();
+    }
+
+   
         
+
+
     [HttpPost]
     [Route("/person")]
     public ActionResult GetPerson(Person person)
@@ -13,28 +33,45 @@ public class PersonController: ControllerBase
         FakeDB.db.Add(person);
         return Ok( FakeDB.db);
     }
-}
 
+    public Person DeletePerson([FromRoute] int id)
+    {
+        var person = FakeDB.db.First(p => p.Id == id);
+        FakeDB.db.Remove(person);
+        return person;
+        
+    }
+}
 public static class FakeDB
 {
     public static List<Person> db = new List<Person>();
 }
 
+
+
+
 public class Person
 {
    public string Name{get;set;}
    public int Age{get;set;}
+   
+   public int Id{get;set;}
 
    public Person()
    {
        
    }
 
-   public Person(string name, int age)
-   {
+   public Person(string name, int age,int id)
+   { 
+       Id = id;
       Name=name;
       Age=age;
    }
+   
+   
+   
+   
    
    
 }
