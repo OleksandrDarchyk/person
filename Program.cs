ec2be630da1f97295de6a3cca4b5fc3737cfe436
OleksandrDarchyk <oledar01@easv365.dk>
2025-09-10 21:43:54 +0200

    Transfer Objects, Dependency Injection, Server side Data Validation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using ph;
+using ph.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSingleton<FakeDB>();// один рбєкт на весь застосунок
+
+builder.Services.AddScoped<IPersonService, PersonServise>();//створюється кожен запит
+
+
+
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApiDocument();
